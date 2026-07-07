cf5dacc2e060fe034bba1cc02aea5838f85823a3
divyanshtare <divyansh.tare@gmail.com>
2026-07-07 12:04:56 -0700

    Initial DWM3001 learning project

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+#define SLEEP_TIME_MS 10000
+
+/* Get LED0 from the board's devicetree */
+static const struct gpio_dt_spec led =
+    GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+
+int main(void)
+{
+    if (!gpio_is_ready_dt(&led)) {
+        return 0;
+    }
+
+    gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+
+    while (1) {
+        gpio_pin_toggle_dt(&led);
+        k_msleep(SLEEP_TIME_MS);
+    }
+
+    return 0;
+}
